b379292b67f0c889c27a68350a0bcf82893498c5
Muhammad Kalmani <muhammad.kalmani@student.manchester.ac.uk>
2024-11-02 14:43:41 +0000

    Messaging functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import Navbar from './components/Navbar.jsx';
+import Navbar from './components/navbar.jsx';
+import Messaging from './components/Messaging.jsx';
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Messaging />
       <h1>Hello, World!</h1>
     </>
   )
