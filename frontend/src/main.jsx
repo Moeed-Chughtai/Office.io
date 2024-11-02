be982a68f2ec67ede86163d220a75296d9dbeb42
Moeed Chughtai <moeed.chughtai@outlook.com>
2024-11-02 14:59:02 +0000

    changes to config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
+import Chat from './components/chat.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Chat />
   </StrictMode>,
 )
