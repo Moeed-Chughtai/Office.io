4ef5ceea40518976aac9f4a1438283b57e1b584b
Muhammad Kalmani <muhammad.kalmani@student.manchester.ac.uk>
2024-11-02 18:16:41 +0000

    Added slider for messaging page between contacts and messages

--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
 import ConversationList from '../functions/ConversationList';
 import MainChatPanel from './MainChatPanel';
+import ResizablePanel from '../functions/ResizablePanel';
 
 function Messaging() {
   const [selectedConversationId, setSelectedConversationId] = useState(null);
 
   return (
     <div className="h-screen bg-gray-100 flex">
-      {/* Sidebar for Conversation List */}
-      <div className="w-1/3 bg-white border-r border-gray-200">
-        <ConversationList onSelectConversation={setSelectedConversationId} />
-      </div>
-      
-      {/* Main Chat Panel */}
-      <div className="w-2/3 flex flex-col">
-        {selectedConversationId ? (
-          <MainChatPanel conversationId={selectedConversationId} />
-        ) : (
-          <div className="flex items-center justify-center h-full text-gray-500">
-            Select a conversation to start chatting
-          </div>
-        )}
-      </div>
+      <ResizablePanel>
+        {/* Sidebar for Conversation List */}
+        <div className="bg-white border-r border-gray-200 h-full">
+          <ConversationList onSelectConversation={setSelectedConversationId} />
+        </div>
+        
+        {/* Main Chat Panel */}
+        <div className="flex flex-col h-full">
+          {selectedConversationId ? (
+            <MainChatPanel conversationId={selectedConversationId} />
+          ) : (
+            <div className="flex items-center justify-center h-full text-gray-500">
+              Select a conversation to start chatting
+            </div>
+          )}
+        </div>
+      </ResizablePanel>
     </div>
   );
 }
