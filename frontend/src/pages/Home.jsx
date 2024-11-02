a1282b84f9e8ff60f235e586a9eaca5773f17afe
HamzaKhan47 <hamza.kh2467@gmail.com>
2024-11-02 21:02:39 +0000

    Scene configured in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import Navbar from '../components/Navbar.jsx';
 import Logo from '../components/Logo.jsx';
+import Scene from '../components/3d/Scene.jsx';
+import { Canvas } from '@react-three/fiber';
 
 const Home = () => {
 
     return (
         <>
             <Navbar />
+            <Canvas style = {{width: '100%', height: '100vh'}}>
+                <Scene/>
+            </Canvas>
         </>
     );
 }
