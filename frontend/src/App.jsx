64a06eec8ea1deca9169aa553ee909b468000cb0
j69145te <taha.ejaz-2@student.manchester.ac.uk>
2024-11-02 14:08:00 +0000

    Added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import Navbar from './components/navbar'
+import { useState } from 'react';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
 
@@ -11,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
