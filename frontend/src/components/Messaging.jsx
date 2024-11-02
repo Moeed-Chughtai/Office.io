d652e142332c56fb4c1bf2c43c8ebfc15bc722c7
Muhammad Kalmani <muhammad.kalmani@student.manchester.ac.uk>
2024-11-02 15:13:19 +0000

    Messaging popup completed

--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -5,12 +5,9 @@ import ChatPanel from './ChatPanel';
 function Messaging() {
     return (
         <div className="h-screen bg-gray-100 flex">
-          {/* Sidebar for Conversation List */}
             <div className="w-1/3 bg-white border-r border-gray-200">
                 <ConversationList />
             </div>
-          
-          {/* Main Chat Panel */}
             <div className="w-2/3 flex flex-col">
                 <ChatPanel />
             </div>
