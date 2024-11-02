4121f10c11c4c54d3ab21c43b4d42ecdafd06737
Muhammad Kalmani <muhammad.kalmani@student.manchester.ac.uk>
2024-11-02 18:23:41 +0000

    removed duplicate app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
 import Messaging from './components/Messaging.jsx'
-import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
