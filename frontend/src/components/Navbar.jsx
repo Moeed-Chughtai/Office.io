86ffa500d59e87259e4eb81492c3fefe20084c1d
j69145te <taha.ejaz-2@student.manchester.ac.uk>
2024-11-02 21:39:07 +0000

    Made navbar item change colour when selected

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,29 @@
-import * as React from 'react';
+import { React, useState, useEffect } from 'react';
 import '../css/Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from './Logo.jsx';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
+  const [activeItem, setActiveItem] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    setActiveItem(location.pathname)
+  }, [location]);
 
   return (
     <>
       <Logo />
       <nav>
-        <Link to='/' className='navbar-item'>Home</Link>
-        <Link to='/schedule' className='navbar-item'>Schedule</Link>
-        <Link to='/messages' className='navbar-item'>Messages</Link>
-        <Link to='/' className='navbar-item'>Contact</Link>
-        <Link to='/login' className='navbar-item'>Sign In</Link>
-        <div class="animation start-home"></div>
+        <Link to='/' className={`navbar-item ${activeItem === '/' ? 'active' : ''}`}>Home</Link>
+        <Link to='/schedule' className={`navbar-item ${activeItem === '/schedule' ? 'active' : ''}`}>Schedule</Link>
+        <Link to='/messages' className={`navbar-item ${activeItem === '/messages' ? 'active' : ''}`}>Messages</Link>
+        <Link to='/' className={`navbar-item ${activeItem === '/jjk' ? 'active' : ''}`}>Contact</Link>
+        <Link to='/' className={`navbar-item ${activeItem === '/jbjc' ? 'active' : ''}`}>Sign Up</Link>
+        <div class="animation"></div>
       </nav>
     </>
   );
