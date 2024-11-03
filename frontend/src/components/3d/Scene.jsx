2995e6695b58672925ea0ec2c65358b7dee94108
HamzaKhan47 <hamza.kh2467@gmail.com>
2024-11-03 07:44:41 +0000

    Added lost delted 2nd room

--- frontend/src/components/3d/Scene.jsx
+++ frontend/src/components/3d/Scene.jsx
@@ -2,7 +2,7 @@
 import React, { useRef, Suspense, useEffect } from 'react';
 import { useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { SmallWall, Wall, Desk, Chair, Floor, OfficePlant, TallOfficePlant, OfficeDisplay, AdjustableWall, Floor2, DoubleGlassDoors, BoardroomTable } from './OfficeComponents';
+import { SmallWall, Wall, Desk, Chair, Floor, Floor2, OfficePlant, TallOfficePlant, OfficeDisplay, AdjustableWall, DoubleGlassDoors, BoardroomTable } from './OfficeComponents';
 import Avatar from './Avatar';
 import { ConferenceSeating } from './ConferenceSeating';
 import { OfficeDivider } from './OfficeDivider';
@@ -58,44 +58,86 @@ function Scene({ isFirstPerson, onAvatarClick }) {
       <ambientLight />
       <pointLight position={[20, 20, 20]} />
 
+      {/* Floors */}
       <Floor />
+      <Floor2 />
+
+      {/* Walls */}
       <Wall position={[-20, 2.5, 0]} args={[0.2, 5, 40]} />
       <Wall position={[20, 2.5, 0]} args={[0.2, 5, 40]} />
       <Wall position={[0, 2.5, 20]} args={[40, 5, 0.2]} />
       <AdjustableWall position={[12.5, 2.5, -20]} width={15} />
       <AdjustableWall position={[-12.5, 2.5, -20]} width={15} />
+
+      {/* Extra Small Walls */}
+      <SmallWall position={[-5, 2.5, -25]} args={[0.2, 5, 8]} />
+      <SmallWall position={[5, 2.5, -25]} args={[0.2, 5, 8]} />
+      <SmallWall position={[-5, 2.5, -37.5]} args={[0.2, 5, 8]} />
+      <SmallWall position={[5, 2.5, -37.5]} args={[0.2, 5, 8]} />
+
+      {/* Additional Walls */}
+      <Wall position={[-20, 2.5, -40]} args={[0.2, 5, 40]} />
+      <Wall position={[20, 2.5, -40]} args={[0.2, 5, 40]} />
+      <Wall position={[0, 2.5, -60]} args={[40, 5, 0.2]} />
+
+      {/* Furniture */}
       <Desk position={[14, 0.55, 17]} />
       <Chair position={[14, 0.5, 18]} />
       <Desk position={[12, 0.55, 16]} />
       <Chair position={[12, 0.5, 17]} />
       <Desk position={[10, 0.55, 14]} />
       <Chair position={[10, 0.5, 13]} />
-      <SmallWall position={[-10, 2.5, -2]} args={[0.2, 5, 8]} />
-      <SmallWall position={[-4, 2.5, -10]} args={[8, 5, 0.2]} />
       <Desk position={[-8, 0.55, -13]} />
       <Chair position={[-8, 0.5, -14]} />
-      <SmallWall position={[10, 2.5, -2]} args={[0.2, 5, 8]} />
-      <SmallWall position={[4, 2.5, -10]} args={[8, 5, 0.2]} />
       <Desk position={[8, 0.55, -13]} />
       <Chair position={[8, 0.5, -14]} />
-      <SmallWall position={[-10, 2.5, 6]} args={[0.2, 5, 8]} />
-      <SmallWall position={[-6, 2.5, 10]} args={[8, 5, 0.2]} />
-      <SmallWall position={[10, 2.5, 6]} args={[0.2, 5, 8]} />
-      <SmallWall position={[6, 2.5, 10]} args={[8, 5, 0.2]} />
       <Desk position={[8, 0.55, 13]} />
       <Chair position={[8, 0.5, 14]} />
+
+      {/* Dividers */}
       <OfficeDivider position={[15, 1, 2]} scale={1.5} />
       <OfficeDivider position={[-15, 1, 2]} scale={1.5} />
+
       <group position={[0, 0, -15]} rotation={[0, Math.PI / 2, 0]}>
         <OfficeDivider position={[-30, 1, 0]} scale={1.5} />
       </group>
+
+      {/* Plants */}
       <OfficePlant position={[-2, 0, 5]} scale={1.5} />
       <TallOfficePlant position={[-9, 0, 12]} scale={1.5} />
+
+      {/* Displays */}
       <OfficeDisplay position={[-6, 3, 9.5]} scale={4} />
       <OfficeDisplay position={[6, 3, 9.5]} scale={4} />
+
+      {/* Conference Seating */}
       <ConferenceSeating position={[0, 0, 5]} scale={1.5} />
       <ConferenceSeating position={[-5, 0, -5]} scale={1.5} />
       <ConferenceSeating position={[5, 0, -5]} scale={1.5} />
+
+      {/* Boardroom Table */}
+      <BoardroomTable position={[-12.5, 2.5, -40]} />
+      <BoardroomTable position={[12.5, 2.5, -40]} />
+
+      {/* Doors */}
+      <DoubleGlassDoors position={[0, 0, -20]} scale={5} />
+
+      {/* Miscellaneous Furniture */}
+      <Sofa position={[0, 0.5, -15]} />
+      <LoungeArea position={[15, 0, -17]} scale={1.5} />
+      <PoolTable position={[-15, 0, 7.5]} scale={2} />
+
+      <group position={[0, 0, 0]} rotation={[0, Math.PI / 5, 0]}>
+        <Sofa position={[-6, 0.5, -5]} scale={1.5} />
+        <Sofa position={[-8, 0.5, -2]} scale={1.5} />
+      </group>
+
+      <group position={[0, 0, 0]} rotation={[0, -Math.PI / 5, 0]}>
+        <Sofa position={[8, 0.5, -1]} scale={1.5} />
+        <Sofa position={[5.5, 0.5, -4]} scale={1.5} />
+      </group>
+
+      {/* Avatars */}
       <Suspense fallback={null}>
         <Avatar ref={avatarRef} isFirstPerson={isFirstPerson} />
       </Suspense>
